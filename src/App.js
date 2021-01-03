1d75b9ccc1aa6bbc120362024c78dff2e37b5dfa
Qi Cao <alanqicao@gmail.com>
2021-01-03 14:20:00 -0700

    setUpBanner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './App.css';
+import Header from "./Header";
+import Home from "./Home";
+
 
 function App() {
   return (
     <div className="app">
-    <h1>Hello Programmers</h1>
-    {/** Header */}
-    {/** Home */}
+   
+    <Header />
+    
+    <Home />
     </div>
   );
 }
