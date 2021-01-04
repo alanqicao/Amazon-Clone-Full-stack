bd99c680e272ef837602fe53f77d6f76a4b43ba6
Qi Cao <alanqicao@gmail.com>
2021-01-03 18:12:48 -0700

    init checkout page

--- src/Product.js
+++ src/Product.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import "./Product.css"
+import React from "react";
+import "./Product.css";
 
-
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
-    <div className="product"> 
-        <div className="product__info">
-          <p> The lean startup</p>
-          <p className="product__price">
-            <small>$</small>
-            <strong>19.99</strong>
-          </p>
-          <div className="prouct__rating">
-            <p>⭐</p>
-           
-          </div>
+    <div className="product">
+      <div className="product__info">
+        <p> {title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" alt=""/>
+      </div>
+      <img
+        src={image}
+        alt=""
+      />
       <button>Add to Basket</button>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
