c4f3d4ecd0c70a32efa27c0d796bccc9963f4439
Qi Cao <alanqicao@gmail.com>
2021-01-04 01:59:19 -0700

    Remove Product

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import "./Checkout.css";
 import Subtotal from "./Subtotal"
+import {useStateValue} from "./StateProvider"
+import CheckoutProduct from "./CheckoutProduct"
+
 const Checkout = () => {
+
+  const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -12,7 +18,18 @@ const Checkout = () => {
            <h2 className="checkout__title">
              Your Shopping Basket
            </h2>
-           {/** basket item */}
+           {basket.map(item=>(
+             <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+             />
+           ))} 
+            
+          
+          
          </div>
       </div>
       <div className="checkout__right">
