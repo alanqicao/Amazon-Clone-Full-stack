4da8fad75da2802daed30e0e72b7eccdc5889454
Qi Cao <alanqicao@gmail.com>
2021-01-10 22:25:38 -0700

    login

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
+
         <Switch>
+          <Route path = '/Login'>
+            <Login />
+          </Route>
           <Route path="/checkout">
+        <Header />
             <Checkout />
           </Route>
           <Route path="/">
+          <Header />
             <Home />
           </Route>
         </Switch>
